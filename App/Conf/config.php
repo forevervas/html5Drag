551622e8075187513d763cddd2665a750f2653fa
xhq <1434970057@qq.com>
2016-02-15 21:25:41 +0800

    pass

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -7,7 +7,7 @@ return array(
 	'DB_HOST'      =>  'localhost',     // 服务器地址
 	'DB_NAME'      =>  'think',     // 数据库名
 	'DB_USER'      =>  'root',     // 用户名
-	'DB_PWD'       =>  'root',     // 密码
+	'DB_PWD'       =>  'forevervas',     // 密码
 	'DB_PORT'      =>  '3306',     // 端口
 	'DB_PREFIX'    =>  'thinks_',     // 数据库表前缀
     'VAR_FILTERS'  =>  'htmlspecialchars'
